a4bfe42f2fe1c31899e8f7dd9a9c3ee4c071e728
ELPROFEALVISO <alejandro.alviso@tec.mx>
2025-09-15 15:30:00 -0600

    Optimizacion de codigo para mecanum drive y widgets elastic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,13 +1,13 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
+
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -17,87 +17,97 @@ import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import com.studica.frc.AHRS;
 
+
 /**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * The methods in this class are called automatically corresponding to each
+ * mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the
+ * package after creating
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
+  private static final String kDefaultAuto = "Default Auto";
+  private static final String kCustomAuto = "My Auto Code";
+
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private final SparkMax leftmotor1 = new SparkMax(5, MotorType.kBrushed);//front
-  private final SparkMax leftmotor2 = new SparkMax(3, MotorType.kBrushed);
-  private final SparkMax rightmotor1 = new SparkMax(4, MotorType.kBrushed);//front 
-  private final SparkMax rightmotor2 = new SparkMax(2, MotorType.kBrushed);
+  private final SparkMax frontLeftMotor = new SparkMax(5, MotorType.kBrushed);
+  private final SparkMax rearLeftMotor = new SparkMax(3, MotorType.kBrushed);
+  private final SparkMax frontRightMotor = new SparkMax(4, MotorType.kBrushed);
+  private final SparkMax rearRightMotor = new SparkMax(2, MotorType.kBrushed);
+
+  private final SparkMaxConfig frontLeftMotorConfig = new SparkMaxConfig();
+  private final SparkMaxConfig rearLeftMotorConfig = new SparkMaxConfig();
+  private final SparkMaxConfig frontRightMotorConfig = new SparkMaxConfig();
+  private final SparkMaxConfig rearRightMotorConfig = new SparkMaxConfig();
+
+  private final PS4Controller driverController = new PS4Controller(0);
+
+  // private AHRS navx = new AHRS(AHRS.NavXComType.kUSB)
+  private final AHRS navx = new AHRS(AHRS.NavXComType.kMXP_SPI);
+
+  private final MecanumDrive mecanumDrive = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+
+  private boolean fod; // Habilitar o deshabilitar el control Field Oriented Drive.
+
+  Timer cronos = new Timer(); //Nuevo timer para cronometrar tiempo de autonomo.;
   
-  private final SparkMaxConfig leftMotor1 = new SparkMaxConfig();//frente
-  private final SparkMaxConfig leftMotor2 = new SparkMaxConfig();
-  private final SparkMaxConfig rightMotor1 = new SparkMaxConfig();//frente
-  private final SparkMaxConfig rightMotor2 = new SparkMaxConfig();
-
-
-  private final PS4Controller controller = new PS4Controller(0);
-
-  private AHRS navx = new AHRS(AHRS.NavXComType.kMXP_SPI);
-
-  MecanumDrive mecanumDrive = new MecanumDrive(leftmotor1, leftmotor2, rightmotor1, rightmotor2);
-
-  boolean fod;        //Habilitar o deshabilitar el control Field Oriented Drive.
-  //private AHRS navx = new AHRS(AHRS.NavXComType.kUSB)
-  
-
-
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
-  public Robot() {
+  public void robotInit() {
+
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choserr", m_chooser);
 
-
-
-    leftMotor1.inverted(false).idleMode(IdleMode.kBrake);// frente
-    leftMotor2.inverted(false).idleMode(IdleMode.kBrake);
-    rightMotor1.inverted(true).idleMode(IdleMode.kBrake);//frente
-    rightMotor2.inverted(true).idleMode(IdleMode.kBrake);
-
-    leftmotor1.configure(leftMotor1, null, null);
-    leftmotor2.configure(leftMotor2, null, null);
-    rightmotor1.configure(rightMotor1, null, null);
-    rightmotor2.configure(rightMotor2, null, null);
-
-
-
-
     
+    frontLeftMotorConfig.inverted(false).idleMode(IdleMode.kBrake);
+    rearLeftMotorConfig.inverted(false).idleMode(IdleMode.kBrake);
+    frontRightMotorConfig.inverted(true).idleMode(IdleMode.kBrake);
+    rearRightMotorConfig.inverted(true).idleMode(IdleMode.kBrake);
 
+    frontLeftMotor.configure(frontLeftMotorConfig,SparkBase.ResetMode.kResetSafeParameters ,SparkBase.PersistMode.kPersistParameters);
+    rearLeftMotor.configure(rearLeftMotorConfig, SparkBase.ResetMode.kResetSafeParameters ,SparkBase.PersistMode.kPersistParameters);
+    frontRightMotor.configure(frontRightMotorConfig, SparkBase.ResetMode.kResetSafeParameters ,SparkBase.PersistMode.kPersistParameters);
+    rearRightMotor.configure(rearRightMotorConfig, SparkBase.ResetMode.kResetSafeParameters ,SparkBase.PersistMode.kPersistParameters);
 
+    mecanumDrive.setDeadband(0.02);
 
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
    * chooser code above as well.
    */
   @Override
@@ -105,6 +115,12 @@ public class Robot extends TimedRobot {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
+
+    cronos.start();
+    cronos.reset();
+    navx.reset();
+
+    
   }
 
   /** This function is called periodically during autonomous. */
@@ -115,8 +131,19 @@ public class Robot extends TimedRobot {
         // Put custom auto code here
         break;
       case kDefaultAuto:
-      default:}
-        // Put default auto code here    }
+
+         if (cronos.get() <= 3){
+
+         mecanumDrive.driveCartesian(0.5, 0,0);
+
+         }else {
+
+          mecanumDrive.driveCartesian(0,0,0);
+
+         }
+      default:
+    }
+    // Put default auto code here }
   }
 
   /** This function is called once when teleop is enabled. */
@@ -132,57 +159,63 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    double ySpeed = controller.getLeftY(); // Remember, this is reversed!
-    double xSpeed = -controller.getLeftX(); // Counteract imperfect strafing
-    double zRotation = -controller.getRightX();
+    double ySpeed = driverController.getLeftY(); // Remember, this is reversed!
+    double xSpeed = -driverController.getLeftX(); // Counteract imperfect strafing
+    double zRotation = -driverController.getRightX();
 
     fod = SmartDashboard.getBoolean("FOD", fod);
 
-     
-
-    SmartDashboard.putNumber("Navx Angle", navx.getAngle());
-
-    if (fod) {
-         
-      //POV
-       
-        mecanumDrive.driveCartesian(ySpeed, xSpeed, zRotation,Rotation2d.fromDegrees(navx.getAngle())); //manejo con navx FOD
-       
-    } else {
-
-      mecanumDrive.driveCartesian(ySpeed, xSpeed, zRotation); //manejo sin navx NO FOD
-    }
-
+    SmartDashboard.putData("Navx Angle", navx);
+    SmartDashboard.putData("Chasis", mecanumDrive);
     
 
-    if (controller.getOptionsButton() == true){
+    if (fod) {
+
+      Rotation2d navXAngle = Rotation2d.fromDegrees(navx.getAngle());
+
+      // POV
+     
+      mecanumDrive.driveCartesian(ySpeed, xSpeed, zRotation, navXAngle); // manejo con
+                                                                                                       // navx FOD
+
+    } else {
+
+      mecanumDrive.driveCartesian(ySpeed, xSpeed, zRotation); // manejo sin navx NO FOD
+    }
+
+    if (driverController.getOptionsButton() == true) {
       navx.reset();
     }
 
-
-  } 
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
